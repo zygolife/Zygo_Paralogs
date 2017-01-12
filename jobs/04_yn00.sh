a18db9fbb4542a3574f15bd2ba9bd2195395a4b5
Jason Stajich <jason@bioperl.org>
2017-01-12 10:59:09 -0800

    update for local install and better param variables

---
+++ jobs/04_yn00.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=0:10:00 
+#SBATCH --job-name=yn00.cds
+N=$SLURM_ARRAY_TASK_ID
+if [ ! $N ]; then
+ N=$1
+fi
+if [ ! $N ]; then
+ echo "Need a cmdline or PBS_ARRAYID value"
+ exit
+fi
+SUBOPT=subopt-kaks/bin
+INFILE=prefix_dirs.txt
+INDIR=pairs
+pairdir=`sed -n ${N}p $INFILE`
+DIR=kaks
+if [ ! -d $DIR ]; then
+ mkdir -p $DIR
+fi
+
+if [ ! -f $DIR/$pairdir.yn00.tab ]; then
+ cat lib/header > $DIR/$dir.yn00.tab
+ $SUBOPT/yn00_cds_prealigned $INDIR/$pairdir/*.cds.fasaln | grep -v kappa >> $DIR/$pairdir.yn00.tab
+else
+ echo "not running $dir, $DIR/$pairdir.yn00.tab already exists"
+fi
