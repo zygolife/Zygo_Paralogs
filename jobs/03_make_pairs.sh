d83359bc4f640e814f788e776d78a0369f100558
Jason Stajich <jason@bioperl.org>
2017-01-12 10:54:30 -0800

    rename to ordering

--- jobs/03_make_pairs.sh
+++ jobs/03_make_pairs.sh
@@ -5,14 +5,15 @@
 #SBATCH --time=1:00:00
 #SBATCH --job-name=make_pair_CDS
 
-N=$PBS_ARRAYID
+N=${SLURM_ARRAY_TASK_ID}
 if [ ! $N ]; then
  N=$1
 fi
-if [ ! $N ]; then 
- echo "Need a cmdline or PBS_ARRAYID value"
+if [ ! $N ]; then
+ echo "Need a cmdline or SLURM array value"
  exit
 fi
+
 INFILE=fasta_results.txt
 F=`sed -n ${N}p $INFILE`
 perl scripts/make_pair_seqfiles.pl $F
